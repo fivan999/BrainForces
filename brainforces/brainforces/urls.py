4d318adbdd3b19929d63d386b845441ea3a49fff
fi_van <fivan999@yandex.ru>
2023-04-02 19:37:01 +0300

    исправил ошибки

--- brainforces/brainforces/urls.py
+++ brainforces/brainforces/urls.py
@@ -1,8 +1,9 @@
-import django.contrib.sites
+
+import django.contrib.admin
 import django.urls
 
 
 urlpatterns = [
-    django.urls.path('admin/', django.contrib.sites.urls),
+    django.urls.path('admin/', django.contrib.admin.site.urls),
     django.urls.path('', django.urls.include('homepage.urls')),
 ]
