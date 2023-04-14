8e1574ba89931fef8b8d01d2a968be8883197537
maryshirk <b06050382@gmail.com>
2023-04-14 21:59:22 +0300

    add search criteria

--- brainforces/archive/views.py
+++ brainforces/archive/views.py
@@ -11,26 +11,26 @@ class ArchiveView(django.views.generic.ListView):
 
     template_name = 'archive/archive.html'
     context_object_name = 'questions'
-    queryset = quiz.models.Question.objects.get_only_useful_list_fields()
-
-
-class ArchiveSearchView(django.views.generic.ListView):
-    """поиск архивных вопросов"""
-
-    template_name = 'archive/search.html'
-    context_object_name = 'questions'
 
     def get_queryset(self) -> django.db.models.QuerySet:
         queryset = quiz.models.Question.objects.get_only_useful_list_fields()
         searched = self.request.GET.get('searched', '')
-        print(searched)
-        if searched:
+        search_criteria = self.request.GET.get('search_critery', 'all')
+        if searched and search_criteria == 'all':
             queryset = (
                 queryset.filter(
                     django.db.models.Q(name__iregex=searched)
                     | django.db.models.Q(text__iregex=searched)
                     | django.db.models.Q(tags__name__iregex=searched)
                 )
+            ).distinct()
+        elif searched and search_criteria == 'name':
+            queryset = (queryset.filter(name__iregex=searched)).distinct()
+        elif searched and search_criteria == 'text':
+            queryset = (queryset.filter(text__iregex=searched)).distinct()
+        elif searched and search_criteria == 'tags':
+            queryset = (
+                queryset.filter(tags__name__iregex=searched)
             ).distinct()
         return queryset
 
